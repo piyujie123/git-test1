defb270506a39e98ac5f75ec0a82484cae85444e
sh <sh@163.com>
2022-06-11 13:27:52 +0800

    commit 更新了输出master1 test

--- test1/src/com/piyujie/git/java.java
+++ test1/src/com/piyujie/git/java.java
@@ -4,5 +4,6 @@ public class java {
     public static void main(String[] args) {
         System.out.println("爱好");
         System.out.println("爱好1");
+        System.out.println("master1 test");
     }
 }
